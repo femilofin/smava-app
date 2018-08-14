516835486302876e014628fcd3aa135d5a861d60
Oluwafemi Olofintuyi <oluwafemi.t.olofintuyi@gmail.com>
2018-08-14 14:43:57 +0100

    style: change to use consistent variable format

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,19 +13,19 @@ if [ "$TRAVIS_BRANCH" == "develop" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; t
   export CLUSTER="$PROJECT-$ENV"
 fi
 
-if [ "${ENV}" == "dev" ] || [ "${ENV}" == "prod" ]; then
+if [ "$ENV" == "dev" ] || [ "$ENV" == "prod" ]; then
     # login to ECR
-    $(aws ecr get-login --region="${AWS_DEFAULT_REGION}" --no-include-email)
+    $(aws ecr get-login --region="$AWS_DEFAULT_REGION" --no-include-email)
 
     # tag docker image
     echo "tagging docker image"
-    docker tag $APP "${IMAGE_REPO}/${PROJECT}-${APP}-${ENV}:latest"
-    docker tag $NGINX "${IMAGE_REPO}/${PROJECT}-${APP}-nginx-${ENV}:latest"
+    docker tag $APP "$IMAGE_REPO/$PROJECT-$APP-$ENV:latest"
+    docker tag $NGINX "$IMAGE_REPO/$PROJECT-$APP-nginx-$ENV:latest"
 
     # push images to ECR
     echo "pushing images to ECR"
-    docker push "${IMAGE_REPO}/${PROJECT}-${APP}-${ENV}:latest"
-    docker push "${IMAGE_REPO}/${PROJECT}-${APP}-nginx-${ENV}:latest"
+    docker push "$IMAGE_REPO/$PROJECT-$APP-$ENV:latest"
+    docker push "$IMAGE_REPO/$PROJECT-$APP-nginx-$ENV:latest"
 
     # ecs deploy
     ecs deploy --timeout 600 $CLUSTER $SERVICE
