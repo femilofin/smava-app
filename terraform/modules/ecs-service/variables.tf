7726049262c04194356142dc4952ec870ecd7c5b
Oluwafemi Olofintuyi <oluwafemi.t.olofintuyi@gmail.com>
2018-08-14 12:27:15 +0100

    chore: add module for ecs service

---
+++ terraform/modules/ecs-service/variables.tf
@@ -0,0 +1,14 @@
+variable "environment" {}
+variable "project" {}
+variable "app" {}
+variable "domain" {}
+variable "url" {}
+variable "alb_dns_name" {}
+variable "alb_zone_id" {}
+variable "health_check" {}
+variable "http_listener_arn" {}
+variable "http_rule_priority" {}
+variable "application_memory" {}
+variable "vpc_id" {}
+variable "cluster_name" {}
+variable "ecs_iam_role" {}
