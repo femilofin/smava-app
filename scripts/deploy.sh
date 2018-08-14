80a43a258d7050bca4e2ba08d770b2aca95ea09c
Oluwafemi Olofintuyi <oluwafemi.olofintuyi@ehealthnigeria.org>
2018-08-14 14:52:56 +0100

    configure ci/cd with travis (#4)
    * chore: configure ci/cd with travis
    
    * style: change to use consistent variable format

---
+++ scripts/deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+
+export AWS_DEFAULT_REGION="eu-west-1"
+export IMAGE_REPO="506127536868.dkr.ecr.eu-west-1.amazonaws.com"
+export PROJECT="smava"
+export APP="smava-web"
+export NGINX="smava-nginx"
+export SERVICE="$PROJECT-$APP"
+
+if [ "$TRAVIS_BRANCH" == "develop" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  export ENV="dev"
+  export CLUSTER="$PROJECT-$ENV"
+fi
+
+if [ "$ENV" == "dev" ] || [ "$ENV" == "prod" ]; then
+    # login to ECR
+    $(aws ecr get-login --region="$AWS_DEFAULT_REGION" --no-include-email)
+
+    # tag docker image
+    echo "tagging docker image"
+    docker tag $APP "$IMAGE_REPO/$PROJECT-$APP-$ENV:latest"
+    docker tag $NGINX "$IMAGE_REPO/$PROJECT-$APP-nginx-$ENV:latest"
+
+    # push images to ECR
+    echo "pushing images to ECR"
+    docker push "$IMAGE_REPO/$PROJECT-$APP-$ENV:latest"
+    docker push "$IMAGE_REPO/$PROJECT-$APP-nginx-$ENV:latest"
+
+    # ecs deploy
+    ecs deploy --timeout 600 $CLUSTER $SERVICE
+fi
+
